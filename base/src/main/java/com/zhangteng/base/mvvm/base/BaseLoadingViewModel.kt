0cee2f2a52c904b14a67cd1b38403a0d1aab422b
张腾 <763263311@qq.com>
2021-11-10 16:30:04 +0800

    vm增加协程请求网络处理

--- base/src/main/java/com/zhangteng/base/mvvm/base/BaseLoadingViewModel.kt
+++ base/src/main/java/com/zhangteng/base/mvvm/base/BaseLoadingViewModel.kt
@@ -1,6 +1,11 @@
 package com.zhangteng.base.mvvm.base
 
 import com.zhangteng.base.mvvm.livedata.SingleLiveData
+import com.zhangteng.rxhttputils.exception.ApiException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
 
 /**
  * ViewModel的带加载中基类
@@ -8,6 +13,87 @@ import com.zhangteng.base.mvvm.livedata.SingleLiveData
 open class BaseLoadingViewModel : BaseViewModel() {
 
     val loadingChange: UiLoadingChange by lazy { UiLoadingChange() }
+
+    /**
+     *  不过滤请求结果
+     * @param block 请求体
+     * @param error 失败回调
+     * @param complete  完成回调（无论成功失败都会调用）
+     * @param isShowDialog 是否显示加载框
+     */
+    fun launchGo(
+        block: suspend CoroutineScope.() -> Unit,
+        error: suspend CoroutineScope.(ApiException) -> Unit,
+        complete: suspend CoroutineScope.() -> Unit = {},
+        isShowDialog: Boolean = true
+    ) {
+        if (isShowDialog) loadingChange.showLoadingView.call()
+        launchUI {
+            handleException(
+                withContext(Dispatchers.IO) { block },
+                { error(it) },
+                {
+                    loadingChange.dismissLoadingView.call()
+                    complete()
+                }
+            )
+        }
+    }
+
+    /**
+     * 过滤请求结果，其他全抛异常
+     * @param block 请求体
+     * @param success 成功回调
+     * @param error 失败回调
+     * @param complete  完成回调（无论成功失败都会调用）
+     * @param isShowDialog 是否显示加载框
+     */
+    fun <T> launchOnlyResult(
+        block: suspend CoroutineScope.() -> IResponse<T>,
+        success: (T) -> Unit,
+        error: (ApiException) -> Unit,
+        complete: () -> Unit = {},
+        isShowDialog: Boolean = true
+    ) {
+        if (isShowDialog) loadingChange.showLoadingView.call()
+        launchUI {
+            handleException(
+                {
+                    withContext(Dispatchers.IO) {
+                        block().let {
+                            if (it.isSuccess()) it.getResult()
+                            else throw ApiException(Throwable(it.getMsg()), it.getCode())
+                        }
+                    }.also { success(it) }
+                },
+                { error(it) },
+                {
+                    loadingChange.dismissLoadingView.call()
+                    complete()
+                }
+            )
+        }
+    }
+
+
+    /**
+     * 异常统一处理
+     */
+    private suspend fun handleException(
+        block: suspend CoroutineScope.() -> Unit,
+        error: suspend CoroutineScope.(ApiException) -> Unit,
+        complete: suspend CoroutineScope.() -> Unit
+    ) {
+        coroutineScope {
+            try {
+                block()
+            } catch (e: Throwable) {
+                error(ApiException.handleException(e))
+            } finally {
+                complete()
+            }
+        }
+    }
 
     inner class UiLoadingChange {
         //显示加载框
@@ -17,4 +103,19 @@ open class BaseLoadingViewModel : BaseViewModel() {
         val dismissLoadingView by lazy { SingleLiveData<Boolean>() }
     }
 
+    /**
+     * 如果需要框架帮你做脱壳处理请实现它
+     */
+    interface IResponse<T> {
+
+        //抽象方法，用户的基类继承该类时，需要重写该方法
+        fun isSuccess(): Boolean
+
+        fun getResult(): T
+
+        fun getCode(): Int
+
+        fun getMsg(): String
+
+    }
 }
--- base/src/main/java/com/zhangteng/base/mvvm/base/BaseViewModel.kt
+++ base/src/main/java/com/zhangteng/base/mvvm/base/BaseViewModel.kt
@@ -1,8 +1,29 @@
 package com.zhangteng.base.mvvm.base
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 /**
  * ViewModel的基类
  */
-open class BaseViewModel : ViewModel()
+open class BaseViewModel : ViewModel() {
+
+    /**
+     * 所有网络请求都在 viewModelScope 域中启动，当页面销毁时会自动
+     * 调用ViewModel的  #onCleared 方法取消所有协程
+     */
+    fun launchUI(block: suspend CoroutineScope.() -> Unit) = viewModelScope.launch { block() }
+
+    /**
+     * 用流的方式进行网络请求
+     */
+    fun <T> launchFlow(block: suspend () -> T): Flow<T> {
+        return flow {
+            emit(block())
+        }
+    }
+}
