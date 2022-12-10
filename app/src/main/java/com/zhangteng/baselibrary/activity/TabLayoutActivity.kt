d0827e5bf10c7706ece85d0be056317e896c2161
Swing <763263311@qq.com>
2022-12-10 11:48:45 +0800

    添加TabLayout demo

--- app/src/main/java/com/zhangteng/baselibrary/activity/TabLayoutActivity.kt
+++ app/src/main/java/com/zhangteng/baselibrary/activity/TabLayoutActivity.kt
@@ -8,9 +8,13 @@ import com.zhangteng.base.adapter.CommonFragmentAdapter
 import com.zhangteng.base.base.BaseActivity
 import com.zhangteng.base.widget.MyTabLayout
 import com.zhangteng.baselibrary.R
+import com.zhangteng.baselibrary.fragment.BaseDemoFragment
 
 class TabLayoutActivity : BaseActivity() {
     private var tab_layout: MyTabLayout? = null
+    private var tab_layout1: MyTabLayout? = null
+    private var tab_layout2: MyTabLayout? = null
+    private var tab_layout3: MyTabLayout? = null
     private var vp: ViewPager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +24,20 @@ class TabLayoutActivity : BaseActivity() {
 
     override fun initView() {
         tab_layout = findViewById(R.id.tab_layout)
+        tab_layout1 = findViewById(R.id.tab_layout1)
+        tab_layout2 = findViewById(R.id.tab_layout2)
+        tab_layout3 = findViewById(R.id.tab_layout3)
         vp = findViewById(R.id.vp)
         tab_layout?.setupWithViewPager(vp)
+        tab_layout1?.setupWithViewPager(vp)
+        tab_layout2?.setupWithViewPager(vp)
+        tab_layout3?.setupWithViewPager(vp)
         val fragments = ArrayList<Fragment>()
-        fragments.add(Fragment())
-        fragments.add(Fragment())
-        fragments.add(Fragment())
+        fragments.add(BaseDemoFragment())
+        fragments.add(BaseDemoFragment())
+        fragments.add(BaseDemoFragment())
         vp?.adapter =
-            CommonFragmentAdapter(supportFragmentManager, arrayOf("1", "2", "3"), fragments)
+            CommonFragmentAdapter(supportFragmentManager, arrayOf("111111", "111111", "111111"), fragments)
     }
 
     override fun initData() {
